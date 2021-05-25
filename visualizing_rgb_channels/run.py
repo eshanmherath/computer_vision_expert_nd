4ad73a821f3984811f9dd9feeb2f942ad594afba
Eshan M. Herath <eshan.m.herath@gmail.com>
2021-05-25 16:42:27 +0530

    Feat: Color Channel Visualization

---
+++ visualizing_rgb_channels/run.py
@@ -0,0 +1,26 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+image = mpimg.imread('images/wa_state_highway.jpg')
+
+plt.imshow(image)
+plt.show()
+
+# Isolating RGB Channels
+
+r = image[:, :, 0]
+g = image[:, :, 1]
+b = image[:, :, 2]
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
+
+ax1.set_title('R Channel')
+ax1.imshow(r, cmap='gray')
+
+ax2.set_title('G Channel')
+ax2.imshow(g, cmap='gray')
+
+ax3.set_title('B Channel')
+ax3.imshow(b, cmap='gray')
+
+plt.show()
